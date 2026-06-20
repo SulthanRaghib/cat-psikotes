f9d9e8898e68d3a83796d70b137ef756034ca386
SulthanRaghib <raghib.smp@gmail.com>
2026-06-20 12:36:36 +0700

    chore(db): remove obsolete SQLite migration script

--- scripts/migrate.ts
+++
@@ -1,8 +0,0 @@
-import db from '../src/lib/db';
-
-try {
-  db.exec('ALTER TABLE questions ADD COLUMN created_at DATETIME DEFAULT CURRENT_TIMESTAMP');
-  console.log('Kolom created_at berhasil ditambahkan ke tabel questions.');
-} catch (e: any) {
-  console.log('Migrasi diabaikan (mungkin kolom sudah ada):', e.message);
-}
